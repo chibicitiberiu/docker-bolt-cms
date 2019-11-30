693e316f51ee0cb3f43b1413e282a1b6bc4f9df9
Tiberiu Chibici <chibicitiberiu@gmail.com>
2019-11-30 19:47:20 +0200

    Fixed some errors in generate_tags.sh

--- generate_tags.sh
+++ generate_tags.sh
@@ -3,8 +3,7 @@
 # Syntax: generate_tags.sh <version> <url>
 
 git checkout versions
-git pull
-sed -i "s#ARG BOLT_URL=.*#ARG BOLT_URL=$2" Dockerfile
+sed -i "s#ARG BOLT_URL=.*#ARG BOLT_URL=$2#" Dockerfile
 git add Dockerfile
 git commit -m "Version $1"
 git tag -d "$1"
